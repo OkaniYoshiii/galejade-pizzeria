ae44b6032460057e15069bb861b3bd9e19b9dd27
OkaniYoshiii <dragut31620@gmail.com>
2024-10-31 10:24:34 +0100

    Ajout section événements

--- src/frontend/typescript/script.ts
+++ src/frontend/typescript/script.ts
@@ -9,28 +9,26 @@
 }
 
 {
-    const HERO_BANNER = document.getElementById('hero-banner');
-    if(HERO_BANNER !== null) {
-        const IMG_CONTAINER = HERO_BANNER.querySelector('.img-fader');
-        const IMGS = Array.from(HERO_BANNER.getElementsByTagName('img'));
+    const IMG_CONTAINERS = document.querySelectorAll('.img-fader');
+    IMG_CONTAINERS.forEach((imgContainer) => {
+        const IMGS = Array.from(imgContainer.querySelectorAll('.fader-img') as NodeListOf<HTMLElement>) ;
         const TRANSITION_DURATION_IN_MS = 1500;
 
         IMGS.forEach((img) => {
             img.style.transition = 'opacity ' + TRANSITION_DURATION_IN_MS + 'ms';
         })
-
+    
         setInterval(() => {
             IMGS[IMGS.length - 1].style.opacity = '0';
-
+    
             setTimeout(() => {
                 IMGS[IMGS.length - 1].style.opacity = '1';
-
+    
                 IMGS.unshift(IMGS[IMGS.length - 1]);
                 IMGS.pop();
-
-                IMGS.forEach((img) => IMG_CONTAINER?.appendChild(img));
+    
+                IMGS.forEach((img) => imgContainer.appendChild(img));
             }, TRANSITION_DURATION_IN_MS);
         }, 4000);
-    }
-    
+    })
 }
