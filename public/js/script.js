c3069241dc6486922127e894acdf25d6ba8cfc86
OkaniYoshiii <dragut31620@gmail.com>
2024-10-31 09:16:29 +0100

    Ajout header + hero-banner

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log('Bonjour le monde');
+{
+    const NAV_BTN = document.querySelector('.nav__btn');
+    const NAV_LINKS = document.querySelector('.nav__links');
+    if (NAV_BTN instanceof HTMLButtonElement && NAV_LINKS !== null) {
+        NAV_BTN.addEventListener('click', function () {
+            NAV_LINKS.classList.toggle('d-flex');
+        });
+    }
+}
+{
+    const HERO_BANNER = document.getElementById('hero-banner');
+    if (HERO_BANNER !== null) {
+        const IMG_CONTAINER = HERO_BANNER.querySelector('.img-fader');
+        const IMGS = Array.from(HERO_BANNER.getElementsByTagName('img'));
+        const TRANSITION_DURATION_IN_MS = 1500;
+        IMGS.forEach((img) => {
+            img.style.transition = 'opacity ' + TRANSITION_DURATION_IN_MS + 'ms';
+        });
+        setInterval(() => {
+            IMGS[IMGS.length - 1].style.opacity = '0';
+            setTimeout(() => {
+                IMGS[IMGS.length - 1].style.opacity = '1';
+                IMGS.unshift(IMGS[IMGS.length - 1]);
+                IMGS.pop();
+                IMGS.forEach((img) => IMG_CONTAINER?.appendChild(img));
+            }, TRANSITION_DURATION_IN_MS);
+        }, 4000);
+    }
+}
